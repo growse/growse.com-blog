7c9e38f152a047d97f38f431c8e346aaba3a82aa
growse <github@growse.com>
2018-08-03 21:01:48 +0100

    Service worker

--- _assets/js/scripts.js
+++ _assets/js/scripts.js
@@ -85,3 +85,11 @@ hljs.initHighlightingOnLoad();
 growse.search();
 growse.getLocation();
 growse.getPostList();
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js").then(function(registration) {
+        console.log("Service Worker registration successful with scope: ", registration.scope);
+    }).catch(function(err) {
+        console.log("Service Worker registration failed: ", err);
+    });
+}
---
+++ sw.js
@@ -0,0 +1,52 @@
+---
+---
+const staticCachePrefix = "growse.com-cache-";
+const staticCacheName = staticCachePrefix+"v1";
+
+console.log("installing service worker");
+
+const urlsToCache = [
+    "/",
+    "/comments/js/embed.min.js",
+    {% for post in site.posts limit:10 %}
+    "{{ post.url }}",
+    {% endfor %}
+    "{% asset critical.scss @path %}",
+    "{% asset main.scss @path %}",
+    "{% asset opensans-300 @path %}",
+    "{% asset opensans-400 @path %}",
+    "{% asset inconsolata-500 @path %}",
+    "{% asset andada-400 @path %}"
+];
+
+self.addEventListener("install", function(e){
+    self.skipWaiting();
+    e.waitUntil(
+        caches.open(staticCacheName).then(function(cache){
+            return cache.addAll(urlsToCache);
+        })
+    )
+});
+
+self.addEventListener("activate", function(e){
+    e.waitUntil(
+        caches.keys().then(function(cacheNames){
+            return Promise.all(
+                cacheNames.filter(function(cacheName){
+                    return cacheName.startsWith(staticCachePrefix)
+                        && cacheName != staticCacheName;
+                }).map(function(cacheName){
+                    return cache.delete(cacheName);
+                })
+            )ß
+        })
+    )
+});
+
+self.addEventListener("fetch", function(e){
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    )
+});
