b7b3f4fe9ac20125c827e13b5583d8b932a9563b
growse <github@growse.com>
2019-08-10 18:07:46 +0100

    Updated java_home for fpm

--- fpm.sh
+++ fpm.sh
@@ -2,4 +2,4 @@
 JFPM=https://github.com/rholder/jfpm/releases/download/v1.3.3.1/jfpm
 curl -C - -o jfpm -L ${JFPM}
 chmod +x jfpm
-./jfpm "$@"
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/ ./jfpm "$@"
