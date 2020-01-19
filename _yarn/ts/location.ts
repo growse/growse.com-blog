c00b88ea88cfa67c6a0918f496eef57894b62e7e
Andrew Rowson <github@growse.com>
2020-01-19 16:22:24 +0000

    Post list kind of works

--- _yarn/ts/location.ts
+++ _yarn/ts/location.ts
@@ -4,12 +4,12 @@ export class Locator {
     private locationEndpoint = "https://www.growse.com/location/";
 
     public getLocation() {
-        $.getJSON(this.locationEndpoint, function (data) {
+        $.getJSON(this.locationEndpoint).then(data => {
             $(document).ready(function () {
                 $('#location').append(`<p>Last seen floating around near <a href="http://maps.google.com/?q=${data.latitude},${data.longitude}">
                 ${data.name}</a>. ${data.totalDistance.toLocaleString()} miles this year.</p>`);
             });
-        }).then(r => console.log(r));
+        });
     }
 }
 
--- _yarn/ts/posts.ts
+++ _yarn/ts/posts.ts
@@ -1,26 +1,53 @@
-export class Posts {
-    public getPostList(): void {
+import $ from "jquery";
 
-        $.get('/posts.json', function (data) {
-            $(document).ready(function () {
-                data.posts.forEach(function (d) {
-                    $("#articlenav").append("<li data-datestamp=\"" + d.date + "\" data-id=\"" + d.title + "\"><a " + (post_url === d.url ? "class=\"here\"" : "") + " href=\"" + d.url + "\" title=\"" + d.title + "\"><span>" + d.title + "</span></a> </li>");
-                });
-                //Scroll the left nav to the right point.
-                const hereClass = '.here';
-                if ($(hereClass).length > 0) {
-                    const percentagedown = ($(hereClass).position().top / $(window).height()) * 100;
-                    if (percentagedown > 50) {
-                        const value = $('.here').position().top - ($(window).height() / 2) + ($('nav ul li:first').height() / 2);
-                        $(".nano").nanoScroller({
-                            scrollTop: value
-                        });
-                    } else {
-                        $(".nano").nanoScroller();
+interface Post {
+    readonly title: string;
+    readonly date: string;
+    readonly url: string;
+}
+
+export class Posts {
+
+    public getPostList(): void {
+        const hereClass = 'here';
+        $.getJSON('/posts.json').then(data => {
+            $(document).ready(() => {
+                data.posts.forEach(function (result: Object) {
+                    const post: Post = <Post>result;
+                    const li = $("<li>");
+                    const a = $("<a>", {href: post.url, title: post.title});
+                    const span = $("<span>", {text: post.title});
+                    // span.text(post.title);
+                    li.data("datestamp", post.date);
+                    li.data("id", post.title);
+                    if (document.location.pathname.endsWith(post.url)) {
+                        a.addClass(hereClass);
                     }
+                    a.append(span);
+                    li.append(a);
+                    $("#articlenav").append(li);
+                });
+                if (document.location.pathname == "/") {
+                    $("#articlenav > li:first > a").addClass(hereClass)
                 }
             });
-        });
 
+            //Scroll the left nav to the right point.
+
+            if ($(hereClass).length > 0 && $(window).height()) {
+                const windowHeight = $(window).height()!;
+                const percentageDown = ($(`.${hereClass}`).position().top / windowHeight) * 100;
+                if (percentageDown > 50) {
+                    const value = $('.here').position().top - (windowHeight / 2) + ($('nav ul li:first').height()! / 2);
+                    $(".nano").nanoScroller({
+                        scrollTop: value
+                    });
+                } else {
+                    $(".nano").nanoScroller();
+                }
+            }
+        })
     }
 }
+
+
